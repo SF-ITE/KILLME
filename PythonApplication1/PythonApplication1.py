f21ccf29a664665c4595800cd8c3bf9760572c7a
Eric Errickson <hireerrickson@gmail.com>
2020-04-12 12:11:05 -0700

    dropped code

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,1 +1,26 @@
 # this is a test
+
+import itertools
+
+
+rack = input('Enter 9 letters here:')
+candidates= set()
+
+def compute_score(word):
+    return len(word)
+
+for i in range(1, len(rack) +1):
+    for combination in itertools.combinations(rack, i):
+        candidates|= {''.join(perm) for perm in itertools.permutations(combination)}
+
+with open('dictionary.txt') as f:
+    dictionary = {word.strip().lower() for word in f}
+
+
+correct_words = dictionary & candidates
+correct_words = [(word, compute_score(word)) for word in correct_words]
+correct_words.sort(key=lambda word: word[1], reverse=True)
+
+for word, score in correct_words:
+    if score > 5:
+        print(str(score) + ' - ' + word)
